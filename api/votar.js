f7f2ddb8e3883dff6c708f98f60802a198d14294
dilan216u-droid <dilan216u@gmail.com>
2026-07-09 23:35:43 -0400

    votar.js

--- api/votar.js
+++ api/votar.js
@@ -1,6 +1,5 @@
 import { createClient } from "@libsql/client";
 
-// Nueva configuración de la base de datos Turso
 const tursoclient = createClient({
   url: "libsql://eleccionescelania-dysaninc-pixel.aws-us-east-1.turso.io",
   authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3ODM2NTM4NzYsImlkIjoiMDE5ZjRhMGUtMWEwMS03MDA1LTk5NTUtMmVmOTI4ZDg0MDliIiwia2lkIjoiM1lNRk42Sk8yN1R6eDIxdWtOdFRCTnkzbTFZOTFoVVZ6b2JiOXREQnNqQSIsInJpZCI6IjMyNWFlOTI5LTJlMjYtNDhiYi05Yjg5LTQ0NTkzN2VmYzI2YiJ9.-GupWP-uVBYgxZ-UKMwlf-DFbodzlRaH22gKjbkYG_BuUkW591wT3KR1J45tds7h6xqGwctyAspeeQ_ZtKZfBA",
@@ -28,7 +27,8 @@ export default async function handler(req, res) {
     return res.status(200).json({ OK: true });
 
   } catch (error) {
-    console.error("Error en Turso:", error);
+    // Esto enviará el mensaje real del error a los logs de Vercel para saber qué pasa exactamente
+    console.error("Error detallado de Turso:", error);
     return res.status(500).json({ error: 'Error del servidor al registrar el voto.' });
   }
 }
